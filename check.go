db8024e0823e50e7886eeb56f9ffdf10605607bd
masahoshiro <victorique.de.blois@asu.edu>
2016-05-01 02:34:43 +0900

    add handler in case of ban

--- check.go
+++ check.go
@@ -1,12 +1,14 @@
 package main
 
 import (
-        "fmt"
-	"os"
-	"log"	
-        "io/ioutil"
-        "net/http"
-	"flag"
+    "fmt"
+    "os"
+    "log"   
+    "io/ioutil"
+    "net/http"
+    "flag"
+    "github.com/clbanning/mxj"
+
 )
 
 var concurrency *int = flag.Int("c", 10, "Default 10")
@@ -31,37 +33,47 @@ func Get(url string) (content string, statusCode int) {
 
 func main() {
 
-	dstFile,err := os.Create("charid.txt")
-		if err!=nil{
-		fmt.Println(err.Error())    
-		return
-	}   	
-	defer dstFile.Close()
+    dstFile, err := os.Create("charid.txt")
+        if err != nil {
+        logger.Println(err.Error())    
+        return
+    }       
+    defer dstFile.Close()
 
-	flag.Parse()
-	
-	sem := make(chan bool, *concurrency)
+    flag.Parse()
+    
+    // Concurrency
+    sem := make(chan bool, *concurrency)
 
-	baseUrl := "https://api.eve-online.com.cn/eve/characterinfo.xml.aspx?characterid="
+    baseUrl := "https://api.eve-online.com.cn/eve/characterinfo.xml.aspx?characterid="
 
-	for i := 100000000; i < 999999999; i++{
-		sem <- true
-		go func(i int){
-			defer func() { <-sem }()
-			_, code := Get(baseUrl + fmt.Sprintf("%d", i))
-			var check string
-			if code == 200 {
-				dstFile.WriteString(fmt.Sprintf("%d", i) + "\n")
-				check = "yes"
-			} else {
-				check = "no"
-			}
-			logger.Println(fmt.Sprintf("[INFO] Checking %d ... %s", i, check))
-		}(i)
-	}
-	for i := 0; i < cap(sem); i++ {
-		sem <- true
-	}
+    for i := 100000000; i < 999999999; i++{
+        sem <- true
+        go func(i int){
+            defer func() { <-sem }()
+            resp, code := Get(baseUrl + fmt.Sprintf("%d", i))
+            var check, info string
+            m, _ := mxj.NewMapXml([]byte(resp))
+
+            if m["eveapi"] == nil {
+                logger.Println(fmt.Sprintf("[ERRO] Network failure"))
+                os.Exit(10086)
+            }
+
+            if code == 200 {
+                dstFile.WriteString(fmt.Sprintf("%d", i) + "\n")
+                info = fmt.Sprintf("%s", m["eveapi"].(map[string]interface{})["result"].(map[string]interface{})["characterName"])
+                check = "yes"
+            } else {
+                info = fmt.Sprintf("%s %s", m["eveapi"].(map[string]interface{})["error"].(map[string]interface{})["-code"],m["eveapi"].(map[string]interface{})["error"].(map[string]interface{})["#text"])
+                check = "no "
+            }
+            logger.Println(fmt.Sprintf("[INFO] Checking %d ... %s > %s", i, check, info))
+
+        }(i)
+    }
+    for i := 0; i < cap(sem); i++ {
+        sem <- true
+    }
 }
-
 
